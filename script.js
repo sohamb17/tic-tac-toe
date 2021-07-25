7e1b463a0607d53fd1e51c40f5f3fc611ef174d4
sohamb17 <sohambelurgikar17@gmail.com>
2021-07-26 01:57:51 +0530

    Add function to check for game over

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const gameBoard = (() => {
-    let board = ['', '', '', '', '', '', '', '', ''];
+    let board = ['', '', '', 
+                 '', '', '', 
+                 '', '', ''];
     return {board};
 })();
 
@@ -11,6 +13,7 @@ const player = name => {
             gameBoard.board[Array.prototype.indexOf.call(displayController.boardSquares, boardSquare)] = name;
             boardSquare.classList.toggle('disabled');
             displayController.render();
+            displayController.checkGameOver();
             displayController.switchPlayer();
         }
     }
@@ -40,5 +43,60 @@ const displayController = (() => {
         }
     }
     bind();
-    return {boardSquares, render, switchPlayer};
+    const checkGameOver = () => {
+        let gameWon = false;
+        for(let i = 0; i < 3; i++) {
+            // check row
+            let start = 3 * i;
+            if(gameBoard.board[start] === 'X' || gameBoard.board[start] === 'O') {
+                if(gameBoard.board[start] === gameBoard.board[start + 1] && gameBoard.board[start] === gameBoard.board[start + 2]) {
+                    gameWon = true;
+                    gameOver(start, start + 1, start + 2);
+                    break;
+                }
+            }
+            // check column
+            start = i;
+            if(gameBoard.board[start] === 'X' || gameBoard.board[start] === 'O') {
+                if(gameBoard.board[start] === gameBoard.board[start + 3] && gameBoard.board[start] === gameBoard.board[start + 6]) {
+                    gameWon = true;
+                    gameOver(start, start + 3, start + 6);
+                    break;
+                }
+            }
+        }
+        // check two diagonals
+        if(!gameWon) {
+            let start = 0;
+            if(gameBoard.board[start] === 'X' || gameBoard.board[start] === 'O') {
+                if(gameBoard.board[start] === gameBoard.board[start + 4] && gameBoard.board[start] === gameBoard.board[start + 8]) {
+                    gameWon = true;
+                    gameOver(start, start + 4, start + 8);
+                }
+            }
+            start = 2;
+            if(gameBoard.board[start] === 'X' || gameBoard.board[start] === 'O') {
+                if(gameBoard.board[start] === gameBoard.board[start + 2] && gameBoard.board[start] === gameBoard.board[start + 4]) {
+                    gameWon = true;
+                    gameOver(start, start + 2, start + 4);
+                }
+            }
+        }
+        // check Tie
+        if(!gameWon) {
+            if(gameBoard.board.every(boardSquare => (boardSquare === 'X' || boardSquare === 'O'))) {
+                console.log('Tie');
+            }
+        }
+    }
+    const gameOver = (firstSquare, secondSquare, thridSquare) => {
+        console.log(firstSquare, secondSquare, thridSquare);
+        console.log(`Winner: ${gameBoard.board[firstSquare]}`);
+        for(let i = 0; i < boardSquares.length; i++) {
+            if(boardSquares[i].className === 'boardSquare') {
+                boardSquares[i].classList.toggle('disabled');
+            }
+        }
+    }
+    return {boardSquares, render, switchPlayer, checkGameOver};
 })();
